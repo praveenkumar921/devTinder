53de5f98a421d49737eab664f0df14fb40943436
Praveen Kumar <praveen.kumar921@gmail.com>
2025-01-10 14:29:52 +0530

    routing

--- src/app.js
+++ src/app.js
@@ -2,8 +2,20 @@ const express = require("express");
 
 const app = express();
 
+app.get('/user', (req, res) => {
+    res.send({ firstName: "praveen", lastName: "kumar" })
+})
+
+app.post('/user', (req, res) => {
+    res.send( "post data successfully to DB!")
+})
+
+app.delete('/user', (req, res) => {
+    res.send( "delete data successfully to DB!")
+})
+
 app.use('/list', (req, res) => {
-res.send("heelo from list")
+    res.send("heelo from list")
 })
 
 app.listen(7777), () => {
